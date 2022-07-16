a328477821f3a1bc817741546493a176d6d3b197
Chudier <chudierp@gmail.com>
2022-07-16 15:48:55 -0700

    client can connect to server

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
-const express = require('express');
-const app = express();
-const http = require('http');
-const websocketServer = require('websocket').server
-const server = http.createServer(app);
 
-const clients = {}; 
+const http = require("http");
+const app = require("express")();
+app.get("/", (req,res)=> res.sendFile(__dirname + "/index.html"))
+
+app.listen(9091, ()=>console.log("Listening on http port 9091"))
+const websocketServer = require("websocket").server
+const httpServer = http.createServer();
+httpServer.listen(9090, () => console.log("Listening.. on 9090"))
+//hashmap clients
+const clients = {};
+const games = {};
 
 const wsServer = new websocketServer({
-    'httpServer': httpServer
+    "httpServer": httpServer
 })
-wsServer.on('request', request => {
+wsServer.on("request", request => {
+    //connect
     const connection = request.accept(null, request.origin);
-    connection.on('open', () => console.log('opened'))
-    connection.on('close', () => console.log('closed'))
-    connection.on('message', message => {
+    connection.on("open", () => console.log("opened!"))
+    connection.on("close", () => console.log("closed!"))
+    connection.on("message", message => {
         const result = JSON.parse(message.utf8Data)
-        console.log(result)
+        //I have received a message from the client
+        //a user want to create a new game
+        if (result.method === "create") {
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "cells": 20
+            }
+            const payLoad = {
+                "method": "create",
+                "game" : games[gameId]
+            }
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad));
+        }
+
     })
+
     //generate a new clientId
     const clientId = guid();
     clients[clientId] = {
@@ -29,16 +52,12 @@ wsServer.on('request', request => {
     }
     //send back the client connect
     connection.send(JSON.stringify(payLoad))
+
 })
+
 function S4() {
     return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
 }
  
 // then to call it, plus stitch in '4' in the third group
 const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
- 
-
-
-server.listen(3000, () => {
-  console.log('listening on *:3000');
-});
